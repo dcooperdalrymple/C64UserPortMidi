91a09c34caa274c43e428cda2d84f6324ba5b17c
dcooperdalrymple <dcooperdalrymple@gmail.com>
2021-03-10 16:17:34 -0600

    Added hex and midi modes to monitor program.

--- monitor/main.asm
+++ monitor/main.asm
@@ -29,14 +29,79 @@ CIA2DDRA    = $DD02 ; Data direction port A
 CIA2DDRB    = $DD03 ; Data direction port B
 CIA2ICR     = $DD0D ; Interrupt Control Register
 
-CHROUT  = $FFD2
-EOL     = $0D
-EOF     = $03
-CLS     = $93
+; Kernal Screen Functions
+CHROUT      = $FFD2 ; Print character from accumulator to screen
+BDCOL       = $D020 ; Border Color
+BKCOL       = $D021 ; Background Color (0?)
+CHCOL       = $0286 ; Current character Color
+SCINIT      = $FF81 ; Init VIC & clear screen
+CURCOL      = $00D3 ; Cursor column
+CURROW      = $00D6 ; Cursor row
+DECOUT      = $BDCD ; Print accumulator as decimal number
+
+; Kernal Keyboard Functions
+SCNKEY      = $FF9F ; Scan keyboard
+GETIN       = $FFE4 ; Read keyboard buffer
+KEYLEN      = $0289 ; Maximum length of buffer, max 15
+KEYREP      = $028A ; Keyboard repeat switch
+
+; Screen Codes
+EOL         = $0D
+EOF         = $03
+CLS         = $93
+SPACE       = $20
+
+; Keyboard Codes
+KEY_F1      = $85
+KEY_F3      = $86
+KEY_F5      = $87
+KEY_F7      = $88
+KEY_NA      = $00 ; No key was pressed
+
+; Color Codes
+COL_BLACK       = $00
+COL_WHITE       = $01
+COL_RED         = $02
+COL_CYAN        = $03
+COL_PURPLE      = $04
+COL_GREEN       = $05
+COL_BLUE        = $06
+COL_YELLOW      = $07
+COL_ORANGE      = $08
+COL_BROWN       = $09
+COL_PINK        = $0A
+COL_DARK_GREY   = $0B
+COL_GREY        = $0C
+COL_LIGHT_GREEN = $0D
+COL_LIGHT_BLUE  = $0E
+COL_LIGHT_GREY  = $0F
 
 ;===================
 ; Program Constants
 ;===================
+
+; Data Printing
+HEX_LINE_MAX    = #10
+DEC_LINE_MAX    = #8
+
+; MIDI Parsing
+COMM_NOTE_OFF       = #$08
+COMM_NOTE_ON        = #$09
+COMM_PRESSURE       = #$0A
+COMM_CC             = #$0B
+COMM_PC             = #$0C
+COMM_CHAN_PRESSURE  = #$0D
+COMM_PITCH_BEND     = #$0E
+;COMM_SYSTEM         = #$0F
+
+COMM_NOTE_OFF_LEN       = #2
+COMM_NOTE_ON_LEN        = #2
+COMM_PRESSURE_LEN       = #2
+COMM_CC_LEN             = #2
+COMM_PC_LEN             = #1
+COMM_CHAN_PRESSURE_LEN  = #1
+COMM_PITCH_BEND_LEN     = #2
+
 
 ; Variables in RAM
 
@@ -48,10 +113,18 @@ temp    ds 4
     SEG.U zp_vars2
     org $02A7 ; 88 bytes of zero page
 
-hexstr  ds 6
+hexstr  ds 5
 buflen  ds 1
 buffer  ds 16
 index   ds 1
+mode    ds 1 ; Bit #6 & #7: %00 = hex, %01 = dec, %10 = midi
+
+linelen ds 1
+
+mcomm   ds 1 ; Midi Command
+mchan   ds 1 ; Midi Channel
+mlen    ds 1 ; Midi Data Length
+mdata   ds 2 ; Midi Data
 
 ;================
 ; Cart header
@@ -68,6 +141,10 @@ index   ds 1
 
 flag_init:
     sei ; Disable interrupts
+
+    ; Start buffer at 0
+    lda #0
+    sta buflen
 
     ; Set interrupt handler address
     lda #<flag_handler
@@ -93,14 +170,115 @@ port_init:
     lda #0
     sta CIA2DDRB ; Set all as inputs on port b
 
+;=================
+; Keyboard Config
+;=================
+
+key_init:
+
+    ; Keyboard buffer of 1 (kinda disable)
+    lda #1
+    sta KEYLEN
+
+    ; Disable key repeats
+    lda #%01000000
+    sta KEYREP
+
+    ; Set initial mode (hex)
+    lda #%00000000
+    sta mode
+
+    lda #0
+    sta buflen
+    sta linelen
+
 ;=====================
 ; Program Info Header
 ;=====================
 
+screen_init:
+
+    ; Set my preferred colors ;)
+    lda #COL_BLACK
+    sta BDCOL
+    sta BKCOL
+    lda #COL_GREEN
+    sta CHCOL
+
+    ; Set cursor to 0x0
+    lda #0
+    sta CURCOL
+    sta CURROW
+
+screen_clear:
+    lda #SPACE
+    ldx #0 ; loops around to $FF as decrementing
+screen_clear_loop:
+    sta $0400,x
+    sta $0500,x
+    sta $0600,x
+    sta $0700,x
+    dex
+    bne screen_clear_loop
+
 print_info:
-    ldx #<info_data
-    ldy #>info_data
+    ; Print info header data
+    ldx #<info_str
+    ldy #>info_str
     jsr Print
+
+    bit mode
+    bvs print_mode_dec
+    bmi print_mode_midi
+
+print_mode_hex:
+    ldx #<mode_hex_str
+    ldy #>mode_hex_str
+    jmp print_mode
+
+print_mode_dec:
+    ldx #<mode_dec_str
+    ldy #>mode_dec_str
+    jmp print_mode
+
+print_mode_midi:
+    ldx #<mode_midi_str
+    ldy #>mode_midi_str
+
+print_mode:
+    jsr Print
+
+;=====================
+; Keyboard Processing
+;=====================
+
+key_check:
+    jsr SCNKEY
+    jsr GETIN
+
+    cmp #KEY_NA
+    beq buffer_check
+
+    cmp #KEY_F1
+    beq mode_hex
+    cmp #KEY_F3
+    beq mode_dec
+    cmp #KEY_F5
+    beq mode_midi
+
+    jmp buffer_check
+
+mode_hex:
+    lda #0
+    jmp mode_set
+mode_dec:
+    lda #%01000000
+    jmp mode_set
+mode_midi:
+    lda #%10000000
+mode_set:
+    sta mode
+    jmp screen_init ; Reset screen
 
 ;========================
 ; MIDI Buffer Processing
@@ -108,24 +286,115 @@ print_info:
 
 buffer_check:
     lda buflen
-    beq buffer_check
+    beq key_check
 
+    bit mode
+    bvs buffer_dec
+    bmi buffer_midi
+
+buffer_hex:
     ldy #0
     sty index
-buffer_loop:
+buffer_hex_loop:
+
+    ; Ignore system messages
+    lda buffer,y
+    and #$f0
+    cmp #$f0
+    beq buffer_hex_skip
+
     lda buffer,y
     jsr PrintHex
 
+buffer_hex_skip:
     inc index
     ldy index
     cpy buflen
-    bne buffer_loop
+    bne buffer_hex_loop
+
+    jmp buffer_reset
+
+buffer_dec:
+    ldy #0
+    sty index
+buffer_dec_loop:
+
+    ; Ignore system messages
+    lda buffer,y
+    and #$f0
+    cmp #$f0
+    beq buffer_dec_skip
+
+    lda buffer,y
+    jsr PrintDec
+
+buffer_dec_skip:
+    inc index
+    ldy index
+    cpy buflen
+    bne buffer_dec_loop
+
+    jmp buffer_reset
+
+buffer_midi:
+    ldy #0
+    sty index
+buffer_midi_loop:
+    lda buffer,y
+    and #%10000000
+    beq buffer_midi_skip
+
+buffer_midi_status:
+    ; command
+    lda buffer,y
+    and #%01110000 ; remove msb status flag
+    REPEAT 4
+    lsr
+    REPEND
+    sta mcomm
+
+    ; channel
+    lda buffer,y
+    and #$0f
+    sta mchan
+
+    ldy mcomm
+    lda midi_comm_len,y
+    sta mlen
+
+    clc
+    adc index
+buffer_midi_wait:
+    cmp buflen
+    bcs buffer_midi_wait
+
+    ldx #0
+buffer_midi_data:
+    inc index
+    ldy index
+
+    lda buffer,y
+    sta mdata,x
+
+    inx
+    cpx mlen
+    bne buffer_midi_data
+
+    jsr PrintMidi
+
+buffer_midi_skip:
+    inc index
+    ldy index
+    cpy buflen
+    bne buffer_midi_loop
+
+    jmp buffer_reset
 
 buffer_reset:
     lda #0
     sta buflen
 
-    jmp buffer_check
+    jmp key_check ; Go back to keyboard loop after flushing out buffer
 
 ;========================
 ; Flag Interrupt Handler
@@ -146,11 +415,23 @@ flag_handler:
     bit CIA2ICR ; Tell interrupt to reset
     rti ; Return to main process
 
-info_data:
+info_str:
     .byte $0E,#CLS ; Change case & cls
     dc.b "c64 uSERpORT mIDI mONITOR",#EOL
     dc.b "d cOOPER dALRYMPLE",#EOL
     dc.b "v0.1 - 2021.03.10",#EOL,#EOL
+    .byte #EOF
+
+mode_hex_str:
+    dc.b "hEX mODE",#EOL,#EOL
+    .byte #EOF
+
+mode_dec_str:
+    dc.b "dECIMAL mODE",#EOL,#EOL
+    .byte #EOF
+
+mode_midi_str:
+    dc.b "mIDI mODE",#EOL,#EOL
     .byte #EOF
 
 hex_prepend:
@@ -159,6 +440,51 @@ hex_data:
     dc.b "0123456789abcdef"
 hex_append:
     dc.b " "
+
+midi_comm_len:
+    .byte #COMM_NOTE_OFF_LEN
+    .byte #COMM_NOTE_ON_LEN
+    .byte #COMM_PRESSURE_LEN
+    .byte #COMM_CC_LEN
+    .byte #COMM_PC_LEN
+    .byte #COMM_CHAN_PRESSURE_LEN
+    .byte #COMM_PITCH_BEND_LEN
+
+midi_comm_strs:
+    .byte #<midi_comm_note_off_str
+    .byte #>midi_comm_note_off_str
+    .byte #<midi_comm_note_on_str
+    .byte #>midi_comm_note_on_str
+    .byte #<midi_comm_pressure_str
+    .byte #>midi_comm_pressure_str
+    .byte #<midi_comm_cc_str
+    .byte #>midi_comm_cc_str
+    .byte #<midi_comm_pc_str
+    .byte #>midi_comm_pc_str
+    .byte #<midi_comm_chan_pressure_str
+    .byte #>midi_comm_chan_pressure_str
+    .byte #<midi_comm_pitch_bend_str
+    .byte #>midi_comm_pitch_bend_str
+midi_comm_note_off_str:
+    dc.b "nOTE oFF        ",#EOF
+midi_comm_note_on_str:
+    dc.b "nOTE oN         ",#EOF
+midi_comm_pressure_str:
+    dc.b "pRESSURE        ",#EOF
+midi_comm_cc_str:
+    dc.b "cONTROL cHANGE  ",#EOF
+midi_comm_pc_str:
+    dc.b "pROGRAM cHANGE  ",#EOF
+midi_comm_chan_pressure_str:
+    dc.b "cHANNEL pRESSURE",#EOF
+midi_comm_pitch_bend_str:
+    dc.b "pITCH bEND      ",#EOF
+
+midi_chan_str:
+    dc.b "c: ",#EOF
+
+midi_data_str:
+    dc.b "d: ",#EOF
 
     include "routines.asm"
 
--- monitor/routines.asm
+++ monitor/routines.asm
@@ -14,18 +14,18 @@ Print:
     sty temp+1
 
     ldy #0
-.print_chr:
+Print_chr:
     lda (temp),y
     cmp #EOF
-    beq .print_done
+    beq Print_done
     pha
     jsr CHROUT
     pla
 
     iny
-    jmp .print_chr
+    jmp Print_chr
 
-.print_done:
+Print_done:
     rts
 
 ;============================================
@@ -58,13 +58,218 @@ PrintHex:
     sta hexstr+0
     lda hex_append
     sta hexstr+3
-    ;lda #EOL
-    ;sta hexstr+4
     lda #EOF
-    sta hexstr+4 ;5
+    sta hexstr+4
 
     ldx #<hexstr
     ldy #>hexstr
     jsr Print
+
+    ; Check line length to avoid weird looping
+    inc linelen
+    lda linelen
+    cmp #HEX_LINE_MAX
+    bne PrintHex_skip
+
+    jsr PrintLine
+
+PrintHex_skip:
+    rts
+
+;================================================
+; PrintDec
+; -----
+; A - Byte value to be printed as decimal string
+;================================================
+
+PrintDec:
+    sta temp+0
+
+    ; Switch to decimal mode
+    clc
+    sed
+
+    ; Clear result
+    lda #0
+    sta temp+1
+    sta temp+2
+
+    ldx #8 ; bits
+PrintDec_bit:
+    asl temp+0 ; shift out bit
+
+    ; add into result
+    lda temp+1
+    adc temp+1
+    sta temp+1
+
+    ; propagate any carry
+    lda temp+2
+    adc temp+2
+    sta temp+2
+
+    ; repeat for next bit
+    dex
+    bne PrintDec_bit
+
+    ; Back to binary
+    cld
+
+    ldy #2
+    sty temp+3
+PrintDec_loop:
+
+    ; Top nibble
+    lda temp,y
+    and #$f0
+    REPEAT 4
+    lsr
+    REPEND
+    adc #$30
+    pha
+    jsr CHROUT
+    pla
+
+    ; Bottom nibble
+    ldy temp+3
+    lda temp,y
+    and #$0f
+    adc #$30
+    pha
+    jsr CHROUT
+    pla
+
+    dec temp+3
+    ldy temp+3
+    bne PrintDec_loop
+
+    ; Space at the end
+    jsr PrintSpace
+
+    ; Check line length
+    inc linelen
+    lda linelen
+    cmp #DEC_LINE_MAX
+    bne PrintDec_skip
+
+    jsr PrintLine
+
+PrintDec_skip:
+    rts
+
+PrintNibble:
+    REPEAT 4
+    asl
+    REPEND
+    sta temp+0
+
+    ; Switch to decimal mode
+    clc
+    sed
+
+    ; Clear result
+    lda #0
+    sta temp+1
+
+    ldx #4 ; bits
+PrintNibble_bit:
+    asl temp+0 ; shift out bit
+
+    ; add into result
+    lda temp+1
+    adc temp+1
+    sta temp+1
+
+    ; repeat for next bit
+    dex
+    bne PrintNibble_bit
+
+    ; Back to binary
+    cld
+
+    ; Top nibble
+    lda temp+1
+    and #$f0
+    REPEAT 4
+    lsr
+    REPEND
+    adc #$30
+    pha
+    jsr CHROUT
+    pla
+
+    ; Bottom nibble
+    lda temp+1
+    and #$0f
+    adc #$30
+    pha
+    jsr CHROUT
+    pla
+
+    ; Space at the end
+    jsr PrintSpace
+
+    rts
+
+
+PrintLine:
+    lda #EOL
+    pha
+    jsr CHROUT
+    pla
+
+    lda #0
+    sta linelen
+
+    rts
+
+PrintSpace:
+    lda #SPACE
+    pha
+    jsr CHROUT
+    pla
+
+    rts
+
+PrintMidi:
+
+    ; Print MIDI command name
+    lda mcomm
+    asl
+    tay
+    ldx midi_comm_strs,y
+    iny
+    lda midi_comm_strs,y
+    tay
+    jsr Print
+
+    jsr PrintSpace
+
+    ; Print channel nibble
+    ldx #<midi_chan_str
+    ldy #>midi_chan_str
+    jsr Print
+
+    lda mchan
+    jsr PrintNibble
+
+    ; Print data
+    ldx #<midi_data_str
+    ldy #>midi_data_str
+    jsr Print
+
+    ldy #0
+    sty temp+3
+PrintMidi_data:
+    lda mdata,y
+    jsr PrintHex
+
+    inc temp+3
+    ldy temp+3
+    cpy mlen
+    bne PrintMidi_data
+
+    ; Finish up with a line return
+    jsr PrintLine
 
     rts
