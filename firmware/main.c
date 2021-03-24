99803f86b12c9a10a6ae63a3298b8f980ec66bd4
dcooperdalrymple <dcooperdalrymple@gmail.com>
2021-03-24 10:38:37 -0500

    Updated firmware with packet-intelligent mode and user port flag improvements. Still unstable.

--- firmware/main.c
+++ firmware/main.c
@@ -10,8 +10,15 @@
 #include <util/delay.h>
 
 #define UART_RX PB0
-#define BUFFER 3 // Serial buffer (midi package is 3 bytes max)
 #define FLAG PB3
+
+#define MODE_SIMPLE 0
+#define MODE_PACKET 1
+#define MODE        MODE_PACKET
+
+#if MODE == MODE_PACKET
+#define FLAG_DELAY 50
+#endif
 
 #include "uart.h"
 #include "shift.h"
@@ -23,21 +30,52 @@ int main(void) {
 
     // Set up C64 User Port flag
     DDRB |= (1<<FLAG);
-    PORTB &= ~(1<<FLAG); // Keep flag low, active on high
+    PORTB |= (1<<FLAG); // Keep flag high, active on low
 
-    uint8_t s;
+    #if MODE == MODE_SIMPLE
     while (1) {
-        s = uart_getc();
-        data_write(s);
+        data_write(uart_getc());
     }
+    #endif
+
+    #if MODE == MODE_PACKET
+    uint8_t s[3] = {0,0,0};
+    uint8_t c, long_packet;
+    while (1) {
+        // Read first byte: command type (MSB) and midi channel (LSB)
+        s[0] = uart_getc();
+
+        // Wait until command and ignore system messages
+        if ((s[0] & 0b10000000) == 0 || (s[0] & 0xf0) == 0xf0) continue;
+
+        // Determine midi packet length
+        c = s[0] & 0b01110000;
+        long_packet = c != 0b01000000 && c != 0b01010000; // Program Change or Channel Pressure
+
+        // Read data bytes
+        s[1] = uart_getc();
+        if (long_packet) s[2] = uart_getc();
+
+        // Send packet
+        data_write(s[0]);
+        data_write(s[1]);
+        if (long_packet) data_write(s[2]);
+    }
+    #endif
+
 }
 
 void data_write(uint8_t data) {
+    // Ensure that flag is high
+    PORTB |= (1<<FLAG);
+
     // Write data to register
     shift_write(data);
 
-    // Trigger C64 User Port flag interrupt
-    PORTB |= (1<<FLAG);
-    _delay_us(20); // arbitrary delay
+    // Set flag low to signal to C64 that data is ready
     PORTB &= ~(1<<FLAG);
+
+    #ifdef FLAG_DELAY
+    _delay_us(FLAG_DELAY);
+    #endif
 }
