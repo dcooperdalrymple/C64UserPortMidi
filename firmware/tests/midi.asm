23be005ebdf57302a331ae2e4168384ed51f8d83
dcooperdalrymple <dcooperdalrymple@gmail.com>
2021-04-27 10:18:39 -0500

    Improvements to MIDI test. Still needs work on receive interrupt.

--- firmware/tests/midi.asm
+++ firmware/tests/midi.asm
@@ -23,20 +23,58 @@
 .include "midi.inc"
 
 init:
-    ; Set Led as output and start high
+    ; Set Led as output and start low
     sbi DDRB, LED
-    sbi PORTB, LED
+    cbi PORTB, LED
 
     rcall MidiInit
 
-loop:
+wait:
     cpi YL, LOW(SRAM_START)
-    breq loop
+    breq wait
 
-    ldi k, (1<<LED)
-    in tmp, PORTB
-    eor tmp, k
-    out PORTB, tmp
+    ldi XH, HIGH(SRAM_START)
+    ldi XL, LOW(SRAM_START)
+
+read:
+
+    ; Just check the command byte
+    ld tmp, X
+    andi tmp, 0xF0
+
+    cpi tmp, 0x90
+    breq note_on
+
+    cpi tmp, 0x80
+    breq note_off
+
+    rjmp next
+
+note_on:
+    ; Read 2 more bytes
+    rcall MidiRead ; Note
+    rcall MidiRead ; Velocity
+
+    ; Check if velocity is zero (aka note off)
+    ld tmp, X
+    cpi tmp, 0
+    breq note_off_b
+
+    sbi PORTB, LED
+    rjmp next
+
+note_off:
+    ; Read 2 more bytes
+    rcall MidiRead ; Note
+    rcall MidiRead ; Velocity
+
+note_off_b:
+    cbi PORTB, LED
+
+next:
+    inc XL
+    cpse XL, YL
+    rjmp read
 
     rcall MidiClear
-    rjmp loop
+    rjmp wait
